c024682ce7b5757b9b0f3465f6aa15ad234fc741
Soumyadeep20 <111590530+Soumyadeep20@users.noreply.github.com>
2023-12-27 19:53:03 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,32 +1,21 @@
 package com.driver;
 
-public class Main
-{
-    
-   public static class Product{
-     public static int product(int x, int y, int z)
-    {
-      return 0;
+public class Main {
+    public static class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
     }
-    public static int product(int x, int y)
-    {
-      return 0;
+    public static void main(String args[]){
+        Product p = new Product();//creating object of product class
+        p.product(0,0);
+        p.product(0,0,0);
+        p.product(0.0,0.0);
     }
-    public static int product(double x, double y)
-    {
-      return 0;
-    }
-    
-  }
-
-	public static void main(String[] args) {
-		Product p=new Product();
-    p.product(1,0);
-    p.product(1,0,0);
-    p.product(1.08,1.09);
-	}
 }
-
-  
-  
-
